b4fd856d4691e5b8420ac1bc7005e96886619cb3
Alen Basic <admin@alenbasic.com>
2020-08-06 13:59:08 +1000

    Basic to-do app written in React using a stateful and stateless component

---
+++ src/TodoItem.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import './bulma.css';
+
+// using these styles/classes as variables to reduce duplication
+// and also to make it easier to understand the intent of the code
+
+const visibleTodoDiv = { gridArea: "1 / 1 / 2 / 2", alignContent: "center",  display: "flex", flexWrap: "wrap"};
+const hiddenTodoDiv = {visibility:"hidden", gridArea: "1 / 1 / 2 / 2"};
+const baseTodoItemClasses = "content is-large notification ml-3 mr-3 ";
+const baseEditTodoItemClasses = "notification mb-5 ml-3 mr-3 is-primary ";
+
+const TodoItem = (props) => {
+    
+    let todoItemStyle = visibleTodoDiv;
+    let editTodoItemStyle = hiddenTodoDiv;
+
+    let todoItemClasses = baseTodoItemClasses;
+    let editTodoItemClasses = baseEditTodoItemClasses;
+    let currentTodoRef = null;
+    let completedTodoStyle = null;
+
+    // checks if we've entered an "edit mode" or not and shows the correct div accordingly
+
+    if (props.editingTodoItem && props.currentTodoItemId === props.id) {
+      todoItemStyle = hiddenTodoDiv;
+      editTodoItemStyle = visibleTodoDiv;
+      currentTodoRef = props.todoInputRef;
+    }
+
+    // on ticking the complete checkbox, strikes through and sets the text to italic
+
+    if (props.completed) {
+        completedTodoStyle = {textDecoration: "line-through", fontStyle: "italic"}
+    }
+
+    // changes the background color if hovering over an item directly, or its delete button
+
+    if (props.hoverDelete  && props.currentTodoItemId === props.id) {
+        todoItemClasses = baseTodoItemClasses + "is-danger";
+    }  else if (props.hoverItem  && props.currentTodoItemId === props.id && !props.editingTodoItem ){
+        todoItemStyle = {...todoItemStyle, backgroundColor : "#00dcbb"};
+    } else {
+        todoItemClasses = baseTodoItemClasses + "is-primary";
+    }
+
+
+
+    return (
+
+        // this is the magic that allows us to display one div directly on top of another
+        // we do this in order to switch from a display "mode" with a label to an edit "mode"
+        // with an input field
+
+        <div style={{display: "grid",  gridTemplateColumns: "1fr", gridTemplateRows: "1fr"}}>
+
+            {/* this is the "default" display div, it shows a label with the todo item the user added */}
+
+            <div
+                onMouseOver={(event, id) => props.onMouseOverItem(event, props.id)}
+                onMouseLeave={props.onMouseLeaveItem}
+                className={todoItemClasses} style={todoItemStyle}>
+                <input
+                    onChange={(event, id) => props.onCompleteHandler(event, props.id)}
+                    checked={props.completed}
+                    className="mr-5" type="checkbox" />
+                <label style={completedTodoStyle} onDoubleClick={(event, id) => props.onDoubleClickHandler(event, props.id)}>
+                    {props.todoItem}
+                </label>
+                <button
+                    onClick={(event, id) => props.onDeleteToDo(event, props.id)}
+                    onMouseOver={(event, id) => props.onMouseOverDelete(event, props.id)}
+                    onMouseLeave={props.onMouseLeaveDelete}
+                    className="delete">
+                </button>
+            </div>
+
+            {/* this is hidden until a user double clicks on the label of the todo item to edit it, then it becomes visible and the other hidden */}
+
+
+            <div className={editTodoItemClasses} style={editTodoItemStyle}>
+                <input className="input is-large "
+                   ref={currentTodoRef}
+                   onBlur={props.onToDoCompletionHandler}
+                   onKeyDown={props.onToDoCompletionHandler}
+                   onChange={(event, id) => props.onInputChangeHandler(event, props.id)}
+                   value={props.todoItem}
+                />
+            </div>
+
+        </div>
+    );
+};
+export default TodoItem;
---
+++ src/TodoList.js
@@ -0,0 +1,224 @@
+import React, { Component } from 'react';
+import TodoItem from './TodoItem';
+import './bulma.css';
+
+class Todolist extends Component {
+  
+    state = {
+        todoItems: [],
+        editingTodoItem: false,
+        currentTodoItemId: null,
+        newToDo: '',
+        hoverDelete: false,
+        hoverItem: false,
+      }
+
+      // refs used to set focus when adding todos or editing one of them
+
+      currentToDoRef = React.createRef();
+      addToDoItemRef = React.createRef();
+
+      //  load todos if we have uses the application previously
+
+      componentDidMount() {
+        this.addToDoItemRef.current.focus();
+        let todoItems = localStorage.getItem('todoItems');
+
+        if (todoItems) {
+          this.setState({
+            todoItems: JSON.parse(todoItems)
+          });
+        }
+      }
+
+      // set the focus on the right component, and update the local storage with the current todos
+
+      componentDidUpdate() {
+        if (this.state.editingTodoItem){
+          this.currentToDoRef.current.focus();
+        } else {
+          this.addToDoItemRef.current.focus();
+        }
+        localStorage.setItem('todoItems', JSON.stringify(this.state.todoItems));
+      }
+
+      // when a user updates a todo, we find it in the list and update it
+
+      onInputChangeHandler = (event, todoId) => {
+
+        const todoListCopy = [...this.state.todoItems];
+        let modifiedTodoList = []
+         todoListCopy.forEach( todo => {
+          if (todo.id === todoId) {
+            todo.task = event.target.value;
+          }
+          modifiedTodoList.push(todo);
+        });
+        this.setState({todoItems: modifiedTodoList});
+      }
+
+      // when a user is in "edit mode" these are the ways they can exit out of it
+
+      onToDoCompletionHandler = (event) => {
+
+        if (event.key === 'Enter' || event.type === 'blur' || event.key === 'Escape') {
+          this.setState({
+              editingTodoItem: false,
+              currentTodoItemId: null
+            });
+        }
+      }
+
+      // enters an "edit mode" on a todo item
+
+      onDoubleClickHandler = (event,todoId) => {
+        this.setState(
+          {
+            editingTodoItem: true,
+            currentTodoItemId: todoId
+          }
+        );
+      }
+
+      // saves the current value of new the todo being entered
+
+      onNewToDoHandler = (event) => {
+
+        this.setState({
+          newToDo: event.target.value
+        })
+
+      }
+
+      // creates a new todo and  adds it to the list
+
+      onToDoAddHandler = (event) => {
+
+        if (event.key === 'Enter') {
+          const todoListCopy = [...this.state.todoItems];
+          todoListCopy.push({
+            id: Math.floor(Math.random()*10000),
+            task: event.target.value,
+            completed: false
+          });
+          this.setState({
+              todoItems: todoListCopy,
+              newToDo: ''
+            });
+        }
+      }
+
+      // searches for the current todo and toggles the completion checkbox
+
+      onCompleteHandler = (event, todoId) => {
+        let modifiedTodoList = []
+         this.state.todoItems.forEach( todo => {
+          if (todo.id === todoId) {
+            let modifiedTodo = {...todo}
+            modifiedTodo.completed = !modifiedTodo.completed;
+            modifiedTodoList.push(modifiedTodo);
+          } else {
+            modifiedTodoList.push(todo);
+          }
+
+        });
+        this.setState({todoItems: modifiedTodoList});
+      }
+
+      // deletes the todo
+
+      onDeleteToDo = (event, todoId) => {
+        this.setState({todoItems:
+                          this.state.todoItems
+                         .filter( todo => todo.id !== todoId),
+            hoverDelete: false,
+            currentTodoItemId: null
+        });
+      }
+
+    // used to change the color of the todo item when hovering over the delete button
+
+    onMouseOverDelete = (event, todoId) => {
+      this.setState({
+          hoverDelete: true,
+          currentTodoItemId: todoId
+      });
+}
+
+    // sets back to the default color when leaving the delete button
+
+    onMouseLeaveDelete = (event) => {
+        this.setState({
+            hoverDelete: false,
+            currentTodoItemId: null
+        });
+    }
+
+    // used to slightly change the todo item color to show which item is currently being interacted with
+
+    onMouseOverItem = (event, todoId) => {
+        if (!this.state.editingTodoItem)  {
+            this.setState({
+                hoverItem: true,
+                currentTodoItemId: todoId
+            });
+        }
+
+    }
+
+    // sets back to the default color when leaving the item
+
+    onMouseLeaveItem = (event) => {
+        this.setState({
+            hoverItem: false
+        });
+    }
+
+    render() {
+
+        // renders a list of todo components to be rendered in the list
+
+        const todoItems = this.state.todoItems.map(todo => {
+            return ( <TodoItem
+               id={todo.id}
+               completed={todo.completed}
+               key={todo.id}
+               todoItem={todo.task}
+               todoInputRef={this.currentToDoRef}
+               editingTodoItem={this.state.editingTodoItem}
+               currentTodoItemId={this.state.currentTodoItemId}
+               onDoubleClickHandler={this.onDoubleClickHandler}
+               onToDoCompletionHandler={this.onToDoCompletionHandler}
+               onInputChangeHandler={this.onInputChangeHandler}
+               onCompleteHandler={this.onCompleteHandler}
+               onDeleteToDo={this.onDeleteToDo}
+               onMouseOverDelete={this.onMouseOverDelete}
+               onMouseLeaveDelete={this.onMouseLeaveDelete}
+               hoverDelete={this.state.hoverDelete}
+               hoverItem={this.state.hoverItem}
+               onMouseOverItem={this.onMouseOverItem}
+               onMouseLeaveItem={this.onMouseLeaveItem}
+
+            />)
+           });
+
+          return (
+              <section>
+
+                  <div className="columns mt-5">
+                  <div style={{paddingBottom: "0"}} className="column is-three-fifths is-offset-one-fifth notification is-info">
+                    <input  ref={this.addToDoItemRef} className="input has-text-centered mb-5 is-large" placeholder="What is on your to-do list?" onChange={this.onNewToDoHandler} onKeyDown={this.onToDoAddHandler} value={this.state.newToDo}  />
+                      {todoItems}
+                  </div>
+                  </div>
+                  <div className="mt-5 column is-one-third is-offset-one-third notification has-text-centered is-success" >Double click on a to-do to edit it!</div>
+
+              </section>
+
+          );
+        }
+    
+    
+}
+
+export default Todolist;
